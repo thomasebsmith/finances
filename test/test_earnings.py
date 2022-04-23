722e253f39e0396db84f5b645165a9966015ee9d
Thomas Smith <thsm@umich.edu>
2022-04-22 23:58:51 -0400

    Add taxable income tests

--- test/test_earnings.py
+++ test/test_earnings.py
@@ -18,6 +18,26 @@ EARNINGS_1 = Earnings(
 )
 
 
+TAX_POLICY_1 = EarningsTaxPolicy(
+    allow_deductions=False,
+    category=TaxCategory.FEDERAL,
+)
+
+
+TAX_POLICY_2 = EarningsTaxPolicy(
+    allow_deductions=True,
+    category=TaxCategory.STATE,
+    floor=Money.of(92500),
+)
+
+
+TAX_POLICY_3 = EarningsTaxPolicy(
+    allow_deductions=True,
+    category=TaxCategory.LOCAL,
+    ceiling=Money.of(200000),
+)
+
+
 def test_agi():
     """Tests the behavior of Earnings.agi."""
     assert EARNINGS_1.agi(TaxCategory.FEDERAL) == Money.of(91000)
@@ -30,3 +50,10 @@ def test_magi():
     assert EARNINGS_1.magi(TaxCategory.FEDERAL) == Money.of(91100)
     assert EARNINGS_1.magi(TaxCategory.STATE) == Money.of(92200)
     assert EARNINGS_1.magi(TaxCategory.LOCAL) == Money.of(93300)
+
+
+def test_taxable_income():
+    """Tests the behavior of Earnings.taxable_income."""
+    assert EARNINGS_1.taxable_income(TAX_POLICY_1) == Money.of(100000)
+    assert EARNINGS_1.taxable_income(TAX_POLICY_2) == Money.of(92500)
+    assert EARNINGS_1.taxable_income(TAX_POLICY_3) == Money.of(93000)
