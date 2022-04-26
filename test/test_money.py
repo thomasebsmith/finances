dd808e3f277ff16c0d486aed833d93053a15ba22
Thomas Smith <thsm@umich.edu>
2022-04-25 23:59:09 -0400

    Add Money.parse tests

--- test/test_money.py
+++ test/test_money.py
@@ -12,3 +12,12 @@ def test_of():
     assert Money.of(0, 0) == ZERO
     assert Money.of(8765, 30) == Money(876530)
     assert Money.of(-314, 15) == Money(-31415)
+
+
+def test_parse():
+    """Tests the behavior of Money.parse."""
+    assert Money.parse("$100") == Money.of(100)
+    assert Money.parse("$0.34") == Money.of(0, 34)
+    assert Money.parse("$0") == ZERO
+    assert Money.parse("-$0") == ZERO
+    assert Money.parse("-$923.60") == Money.of(-923, 60)
