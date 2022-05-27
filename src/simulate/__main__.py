b134bfcefe6d88d55d9ff319fadb27286a954cd6
Thomas Smith <thsm@umich.edu>
2022-05-26 23:58:53 -0400

    Use argparse in simulate

--- src/simulate/__main__.py
+++ src/simulate/__main__.py
@@ -1,9 +1,14 @@
 """A program for simulating personal finances."""
 
+from argparse import ArgumentParser
+
 
 def main():
     """Simulates personal finances."""
+    parser = ArgumentParser(description="Simulate personal finances.")
+    args = parser.parse_args()
     print("Hello from simulate!")
+    print(args)
 
 
 if __name__ == "__main__":
