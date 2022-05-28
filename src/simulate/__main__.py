adab8722c63188a1f517d803f6733db4ba493f56
Thomas Smith <thsm@umich.edu>
2022-05-27 23:46:04 -0400

    Add start and end year arguments to simulate

--- src/simulate/__main__.py
+++ src/simulate/__main__.py
@@ -6,9 +6,24 @@ from argparse import ArgumentParser
 def main():
     """Simulates personal finances."""
     parser = ArgumentParser(description="Simulate personal finances.")
+    parser.add_argument(
+        "--start",
+        metavar="YEAR",
+        type=int,
+        required=True,
+        help="The year in which to start the simulation",
+    )
+    parser.add_argument(
+        "--end",
+        metavar="YEAR",
+        type=int,
+        required=True,
+        help="The year in which to end the simulation",
+    )
     args = parser.parse_args()
     print("Hello from simulate!")
-    print(args)
+    print(f"Starting in {args.start}")
+    print(f"Ending in {args.end}")
 
 
 if __name__ == "__main__":
