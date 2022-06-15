e7ca2cfd54a2c8daaeaf9fd6dd0006407344463a
Thomas Smith <thsm@umich.edu>
2022-06-14 23:51:16 -0400

    Add grow function

--- src/simulate/growth.py
+++ src/simulate/growth.py
@@ -7,6 +7,13 @@ from finances import Money
 from .errors import SimulationParameterError
 
 
+def _ensure_positive(number: float, description: str):
+    if number <= 0.0:
+        raise SimulationParameterError(
+            f"{description} must be positive but is {number}"
+        )
+
+
 def accumulate_and_grow(
     accumulation_rate: Money, growth_rate: float, time: int
 ) -> Money:
@@ -24,10 +31,7 @@ def accumulate_and_grow(
         time - The number of time periods to simulate.
     Return value: The amount of money accumulated.
     """
-    if growth_rate <= 0.0:
-        raise SimulationParameterError(
-            f"growth_rate must be positive but is {growth_rate}"
-        )
+    _ensure_positive(growth_rate, "growth_rate")
 
     if growth_rate == 1.0:
         return accumulation_rate * time
@@ -35,3 +39,19 @@ def accumulate_and_grow(
     return accumulation_rate.grow_and_round(
         (growth_rate**time - 1) / math.log(growth_rate)
     )
+
+
+def grow(principal: Money, growth_rate: float, time: int) -> Money:
+    """
+    Calculates a principal compounded over time.
+
+    Arguments:
+        principal - The starting amount of money.
+        growth_rate - The rate at which to grow the money each time period. FOr
+                      example, 1.0 means no change.
+        time - The number of time periods to simulate.
+    Return value: The compounded principal.
+    """
+    _ensure_positive(growth_rate, "growth_rate")
+
+    return principal.grow_and_round(growth_rate**time)
