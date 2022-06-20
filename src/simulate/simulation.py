a922d7eb338e4879db5d490566647f76963554b9
Thomas Smith <thsm@umich.edu>
2022-06-19 23:31:06 -0400

    Add doc comment to Simulation init

--- src/simulate/simulation.py
+++ src/simulate/simulation.py
@@ -5,6 +5,13 @@ class Simulation:
     """Represents a financial simulation."""
 
     def __init__(self, start: int, end: int):
+        """
+        Creates a simulation.
+
+        Arguments:
+            start - The year in which the simulation should start, inclusive.
+            end - The year in which the simulation should end, exclusive.
+        """
         self.start = start
         self.end = end
 
@@ -12,4 +19,4 @@ class Simulation:
         """Runs the simulation."""
         print("Hello from simulate!")
         print(f"Starting in {self.start}")
-        print(f"Ending in {self.end}")
+        print(f"Ending before {self.end}")
