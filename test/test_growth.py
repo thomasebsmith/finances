342969b86cb1a2accd243c9af71c3f130ab35c6f
Thomas Smith <thsm@umich.edu>
2022-07-01 21:58:47 -0400

    Use ZERO constant in tests

--- test/test_growth.py
+++ test/test_growth.py
@@ -1,14 +1,14 @@
 """Tests of src/simulate/growth.py."""
 
 from finances import Money
-from finances.money import NEGATIVE_ZERO
+from finances.money import NEGATIVE_ZERO, ZERO
 from simulate.growth import accumulate_and_grow, grow
 
 
 def test_accumulate_and_grow():
     """Tests the behavior of accumulate_and_grow."""
-    assert accumulate_and_grow(Money.of(0), 1.0, 0) == Money.of(0)
-    assert accumulate_and_grow(Money.of(0), 1.0, 10) == Money.of(0)
+    assert accumulate_and_grow(ZERO, 1.0, 0) == ZERO
+    assert accumulate_and_grow(ZERO, 1.0, 10) == ZERO
     assert accumulate_and_grow(Money.of(10), 1.0, 5) == Money.of(50)
 
     assert accumulate_and_grow(Money.of(7, 11), 2.35, 10) == Money.of(42736, 8)
@@ -19,9 +19,9 @@ def test_accumulate_and_grow():
 
 def test_grow():
     """Tests the behavior of grow."""
-    assert grow(Money.of(0), 0.01, 0) == Money.of(0)
-    assert grow(Money.of(0), 123.456, 78) == Money.of(0)
-    assert grow(Money.of(0), 987.6, -5) == Money.of(0)
+    assert grow(ZERO, 0.01, 0) == ZERO
+    assert grow(ZERO, 123.456, 78) == ZERO
+    assert grow(ZERO, 987.6, -5) == ZERO
 
     assert grow(Money.of(22, 33), 1.0, 100) == Money.of(22, 33)
     assert grow(Money.of(-66, 55), 1.0, -98) == Money.of(-66, 55)
