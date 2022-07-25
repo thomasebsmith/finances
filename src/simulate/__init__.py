a9f708e4e98fd237c066cd8594004b019852fec7
Thomas Smith <thsm@umich.edu>
2022-07-24 23:25:04 -0400

    Fully encapsulate Account balances

--- src/simulate/__init__.py
+++ src/simulate/__init__.py
@@ -1,4 +1,5 @@
 """Utilities for simulating personal finances."""
 
 from . import growth
+from .account import Account
 from .simulation import Simulation
--- src/simulate/account.py
+++ src/simulate/account.py
@@ -7,4 +7,21 @@ class Account:
     """Represents a financial account."""
 
     def __init__(self, balance: AddableT):
-        self.balance = balance
+        """Creates an Account."""
+        self._balance = balance
+
+    def balance(self) -> AddableT:
+        """Retrieves the account balance."""
+        return self._balance
+
+    def __iadd__(self, other: AddableT) -> None:
+        """Adds other to this account's balance."""
+        self._balance += other
+
+    def __isub__(self, other: AddableT) -> None:
+        """Subtracts other from this account's balance."""
+        self._balance -= other
+
+    def __imul__(self, other: int) -> None:
+        """Multiplies this account's balance by an integer."""
+        self._balance *= other
