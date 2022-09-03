43446c0c4a3abbf0bcac13fcaf92490a63ff8d66
Thomas Smith <thsm@umich.edu>
2022-09-02 23:56:31 -0400

    Test Probability.__add__

--- test/test_probability.py
+++ test/test_probability.py
@@ -13,3 +13,19 @@ def test_value() -> None:
     assert Probability(Fraction(2, 7)).value() == Fraction(2, 7)
     assert Probability(Fraction(10, 11)).value() == Fraction(10, 11)
     assert Probability(Fraction(1, 2)).value() == Fraction(1, 2)
+
+
+def test_add() -> None:
+    """Tests the behavior of Probability.add."""
+    assert Probability.ZERO + Probability.ONE == Probability.ONE
+    assert Probability.ONE + Probability.ZERO == Probability.ONE
+    assert Probability(Fraction(0)) + Probability.ZERO == Probability.ZERO
+    assert Probability(Fraction(1, 2)) + Probability(
+        Fraction(1, 2)
+    ) == Probability(Fraction(1))
+    assert Probability(Fraction(8, 65)) + Probability(
+        Fraction(29, 39)
+    ) == Probability(Fraction(169, 195))
+    assert Probability(Fraction(1, 100)) + Probability(
+        Fraction(2, 25)
+    ) == Probability(Fraction(9, 100))
