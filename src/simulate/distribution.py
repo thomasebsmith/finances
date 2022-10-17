3c4462b141003b071a40e8b3d3ee8c9c4e898269
Thomas Smith <thsm@umich.edu>
2022-10-16 23:52:49 -0400

    Add Distribution.__add__ function

--- src/simulate/distribution.py
+++ src/simulate/distribution.py
@@ -1,39 +1,45 @@
 """Types and functions for dealing with numeric distributions."""
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import Generic, Protocol, TypeVar
 
 from finances import AddableComparable, Range
 
 
-_RangeT_contra = TypeVar(
-    "_RangeT_contra", bound=AddableComparable, contravariant=True
-)
-_ValueT_co = TypeVar("_ValueT_co", bound=AddableComparable, covariant=True)
+_RangeT = TypeVar("_RangeT", bound=AddableComparable)
+_ValueT = TypeVar("_ValueT", bound=AddableComparable)
 
 
-class Distribution(Protocol, Generic[_RangeT_contra, _ValueT_co]):
+class Distribution(Protocol, Generic[_RangeT, _ValueT]):
     """
     A type that stores numeric distributions.
 
     Types that fulfill this protocol must expose frozen/constant semantics.
     """
 
-    def value(self, at_point: _RangeT_contra) -> _ValueT_co:
+    def value(self, at_point: _RangeT) -> _ValueT:
         """Returns the value at the given point."""
 
-    def average(self, in_range: Range[_RangeT_contra]) -> _ValueT_co:
+    def average(self, in_range: Range[_RangeT]) -> _ValueT:
         """Returns the average value in the given range."""
+
+    def __add__(
+        self, other: Distribution[_RangeT, _ValueT]
+    ) -> Distribution[_RangeT, _ValueT]:
+        """Returns the sum of this distribution and other."""
+        return _SumDistribution(self, other)
 
 
 @dataclass(eq=False, frozen=True)
-class _SumDistribution(Distribution, Generic[_RangeT_contra, _ValueT_co]):
+class _SumDistribution(Distribution, Generic[_RangeT, _ValueT]):
     """A sum of two Distributions."""
 
-    _dist_1: Distribution[_RangeT_contra, _ValueT_co]
-    _dist_2: Distribution[_RangeT_contra, _ValueT_co]
+    _dist_1: Distribution[_RangeT, _ValueT]
+    _dist_2: Distribution[_RangeT, _ValueT]
 
-    def value(self, at_point: _RangeT_contra) -> _ValueT_co:
+    def value(self, at_point: _RangeT) -> _ValueT:
         return self._dist_1.value(at_point) + self._dist_2.value(at_point)
 
-    def average(self, in_range: Range[_RangeT_contra]) -> _ValueT_co:
+    def average(self, in_range: Range[_RangeT]) -> _ValueT:
         return self._dist_1.average(in_range) + self._dist_2.average(in_range)
