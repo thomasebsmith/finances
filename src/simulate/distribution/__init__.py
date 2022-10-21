36752e773d97dad6652d96d3fc0da7a3d738aea3
Thomas Smith <thsm@umich.edu>
2022-10-20 23:55:06 -0400

    Add ConstantDistribution class

--- src/simulate/distribution/__init__.py
+++ src/simulate/distribution/__init__.py
@@ -1,3 +1,4 @@
 """Types and functions for dealing with numeric distributions."""
 
+from .constant import ConstantDistribution
 from .distribution import Distribution
---
+++ src/simulate/distribution/constant.py
@@ -0,0 +1,25 @@
+"""Classes and functions related to constant distributions."""
+
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+from finances import AddableComparable, Range
+
+from .distribution import Distribution
+
+
+_RangeT = TypeVar("_RangeT", bound=AddableComparable)
+_ValueT = TypeVar("_ValueT", bound=AddableComparable)
+
+
+@dataclass(eq=False, frozen=True)
+class ConstantDistribution(Distribution, Generic[_RangeT, _ValueT]):
+    """A distribution with a constant value at every point."""
+
+    constant_value: _ValueT
+
+    def value(self, at_point: _RangeT) -> _ValueT:
+        return self.constant_value
+
+    def average(self, in_range: Range[_RangeT]) -> _ValueT:
+        return self.constant_value
