76220a37b4a6d1c7292f19bebb2d922a4bdbd50a
Thomas Smith <thsm@umich.edu>
2022-11-17 23:41:54 -0500

    Separate utilities into multiple source files

---
+++ src/finances/utilities/__init__.py
@@ -0,0 +1,4 @@
+"""Utility classes and functions that are not directly related to finances."""
+
+from .protocols import Addable, AddableComparable, Comparable, Growable
+from .range import Range
--- src/finances/utilities/range.py
+++ src/finances/utilities/range.py
@@ -1,66 +1,12 @@
-"""Utility classes and functions that are not directly related to finances."""
+"""Classes and functions related to contiguous ranges."""
+
 from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import ClassVar, Generic, Protocol, TypeVar, Union
+from typing import ClassVar, Generic, TypeVar, Union
 
-
-# TODO(Python3.11): use Self instead of this
-AddableT = TypeVar("AddableT", bound="Addable")
-
-
-class Addable(Protocol):
-    """
-    A type that can be added to itself.
-
-    It must have immutable semantics.
-    """
-
-    def __add__(self: AddableT, other: AddableT) -> AddableT:
-        """Adds two of this type."""
-
-    def __sub__(self: AddableT, other: AddableT) -> AddableT:
-        """Subtracts one of this type from this instance."""
-
-    def __mul__(self: AddableT, other: int) -> AddableT:
-        """Multiplies this instance by other."""
-
-
-ComparableT = TypeVar("ComparableT", bound="Comparable")
-
-
-class Comparable(Protocol):
-    """A type that can be compared with itself."""
-
-    def __eq__(self: ComparableT, other: object) -> bool:
-        """Checks whether self is equal to other."""
-
-    def __lt__(self: ComparableT, other: ComparableT) -> bool:
-        """Checks whether self is less than other."""
-
-    def __le__(self: ComparableT, other: ComparableT) -> bool:
-        """Checks whether self is less than or equal to other."""
-
-    def __gt__(self: ComparableT, other: ComparableT) -> bool:
-        """Checks whether self is greater than other."""
-
-    def __ge__(self: ComparableT, other: ComparableT) -> bool:
-        """Checks whether self is greater than or equal to other."""
-
-
-class AddableComparable(Addable, Comparable, Protocol):
-    """A type that is both Addable and Comparable."""
-
-
-GrowableT = TypeVar("GrowableT", bound="Growable")
-
-
-class Growable(AddableComparable, Protocol):
-    """A custom type that can be multiplied by a ratio."""
-
-    def grow_and_round(self: GrowableT, ratio: float) -> GrowableT:
-        """Multiplies this instance by ratio, rounding."""
+from .protocols import Comparable
 
 
 _RangeT = TypeVar("_RangeT", bound=Comparable)
