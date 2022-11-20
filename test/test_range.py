e1ba9e0fae757ae0524b9bc8ac6b5fe7823f9e1f
Thomas Smith <thsm@umich.edu>
2022-11-19 23:34:50 -0500

    Test Range.__post_init__

--- test/test_range.py
+++ test/test_range.py
@@ -1,11 +1,15 @@
 """Tests of src/finances/utilities/range.py."""
 
+from pytest import raises
+
 from finances import Money
 from finances.utilities import Range
 
 
 def test_create() -> None:
     """Tests creating a Range."""
+    assert Range(0, 0).start == 0
+    assert Range(0, 0).end == 0
     assert Range(3, 5).start == 3
     assert Range(3, 5).end == 5
     assert Range(Money.of(-5), Money.of(100, 34)).start == Money.of(-5)
@@ -22,3 +26,13 @@ def test_create() -> None:
         Range(Range.NEGATIVE_INFINITY, Range.POSITIVE_INFINITY).end
         == Range.POSITIVE_INFINITY
     )
+
+
+def test_create_exception() -> None:
+    """Tests creating invalid Ranges."""
+    with raises(ValueError):
+        Range(0, -1)
+    with raises(ValueError):
+        Range(8888888, 0)
+    with raises(ValueError):
+        Range(Money.of(32, 7), Money.of(3, 28))
